582931b345e979f13a650712c78d23728c0a40f0
yeonhan <yeonhanlab@gmail.com>
2026-06-17 14:43:25 +0900

    react-hook-form과 signUpSchema 연결

--- src/pages/auth/signup/signUpPage.tsx
+++ src/pages/auth/signup/signUpPage.tsx
@@ -1,4 +1,16 @@
+import { useForm } from "react-hook-form";
+import { type SignUpInputType, signUpSchema } from "../../../schemas/auth/signUpSchma.ts";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 function SignUpPage() {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<SignUpInputType>({
+        resolver: zodResolver(signUpSchema),
+        mode: "onBlur",
+    });
     return <></>;
 }
 
