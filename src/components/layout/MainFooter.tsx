5da98a5f2d2951e3bba2b6d607d578b8a3044925
yeonhan <yeonhanlab@gmail.com>
2026-06-17 13:18:28 +0900

    signUpSchema 작성

--- src/components/layout/MainFooter.tsx
+++ src/components/layout/MainFooter.tsx
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    background-color: ${props => props.theme.colors.background.paper};
+    border-top: 1px solid ${props => props.theme.colors.divider};
+`;
+
 function MainFooter() {
-    return <>Foot</>;
+    return <FooterContainer></FooterContainer>;
 }
 
 export default MainFooter;
